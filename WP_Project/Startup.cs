b93e3cec14d39c282b5632d4599dcfc40c19ff4f
andrewthomas245 <40845968+andrewthomas245@users.noreply.github.com>
2018-09-01 12:57:45 +0630

    Add project files.

---
+++ WP_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WP_Project.Startup))]
+namespace WP_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
