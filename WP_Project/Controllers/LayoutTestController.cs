ccb9569434b70ce4835e8e580d03cbb8df898842
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-09-17 22:30:52 +0630

    add Layout

---
+++ WP_Project/Controllers/LayoutTestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WP_Project.Controllers
+{
+    public class LayoutTestController : Controller
+    {
+        // GET: LayoutTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LayoutTest
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        // GET: LayoutTest
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        // GET: LayoutTest
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        // GET: LayoutTest
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
